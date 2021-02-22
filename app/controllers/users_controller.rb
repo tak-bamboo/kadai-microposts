39fe4b7a915a1ccdfec65dc5958e40e80fe8c2e4
tak-bamboo <ubuntu@ip-172-31-33-214.ap-northeast-1.compute.internal>
2021-02-22 08:22:03 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :require_user_logged_in, only: [:index, :show]
+    
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -11,6 +13,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+ def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
