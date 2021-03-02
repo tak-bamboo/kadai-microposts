b8ab73af4247d121d55f58f445e6f37838668b7d
tak-bamboo <ubuntu@ip-172-31-33-214.ap-northeast-1.compute.internal>
2021-03-02 14:16:55 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-    before_action :require_user_logged_in, only: [:index, :show]
-    
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
- def create
+  def create
     @user = User.new(user_params)
 
     if @user.save
@@ -27,10 +27,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
 end
